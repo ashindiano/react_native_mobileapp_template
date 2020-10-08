e427758bd5d298919462b057427f07339c213219
Ash Indiano <facetheash@gmail.com>
2020-10-08 23:22:30 +0530

    updated store

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 import * as React from 'react';
 import {AppRegistry} from 'react-native';
-import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {Provider as StoreProvider} from 'react-redux';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {name as appName} from './app.json';
 import App from './src/App';
-
-const theme = {
-  ...DefaultTheme,
-  roundness: 2,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#3498db',
-    secondary: '#fffeee',
-    accent: '#f1c40f',
-  },
-};
+import {store} from './src/_helpers/store';
+import {theme} from './src/_helpers/theme';
 
 export default function Main() {
   return (
-    <PaperProvider theme={theme}>
-      <App />
-    </PaperProvider>
+    <StoreProvider store={store}>
+      <PaperProvider theme={theme}>
+        <App />
+      </PaperProvider>
+    </StoreProvider>
   );
 }
 
